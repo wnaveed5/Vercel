9a61f4dc0e63543e6a88232c82e4ebc8bd43e671
wnaveed5 <wnaveed5@gmail.com>
2025-02-19 18:58:11 -0600

    final pls

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.blueskytd.com', // Replace with your actual site URL
-  generateRobotsTxt: true, // Generate a robots.txt file
-  sitemapSize: 5000, // Max number of URLs per sitemap file
-};
-
