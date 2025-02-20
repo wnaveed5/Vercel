27bba53a3681c8f4d1b84f748918855f022cd1b3
wnaveed5 <wnaveed5@gmail.com>
2025-02-19 21:15:57 -0600

    deleted

--- components/seo.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { NextSeo } from "next-seo";
-
-export default function Seo({ title, description }: { title: string; description: string }) {
-  return <NextSeo title={title} description={description} />;
-}
